a49b5511aba05595a3061a91958d96494d6b6b2c
Averdrian <adrianestevezg@gmail.com>
2023-12-19 15:55:02 +0100

    cond expressions, finished first part

--- imperative_interpreter.hs
+++ imperative_interpreter.hs
@@ -7,13 +7,15 @@ type Program = [Instruction] --un programas es simplemente una lista de instrucc
 
 --tenemos dos tipos de isntrucciones, la asignacion que iguala al identificador dado el valor de la expresion
 --y la expresion condicional, que ejecutara sus instrucciones mientras su condicion sea cierta
-data Instruction = Asign String Expression | While Expression Program
+data Instruction = Asign String Expression | While CondExpression Program
 instance Show Instruction where
     show (Asign s e) = show s ++ " = " ++ show e
 
 infix 1 =:
 (=:) :: String -> Expression -> Instruction
 s =: e = Asign s e
+
+
 
 data Expression = I Int | V String | Add Expression Expression | Sub Expression Expression | Mul Expression Expression
 instance Show Expression where
@@ -22,10 +24,6 @@ instance Show Expression where
     show (Add e1 e2) = show e1 ++ " + " ++ show e2
     show (Sub e1 e2) = show e1 ++ " - " ++ show e2
     show (Mul e1 e2) = show e1 ++ " * " ++ show e2
-
-
-value :: Variable -> Int
-value (_, v) = v
 
 infixl 6 +:
 (+:) :: Expression -> Expression -> Expression 
@@ -41,6 +39,41 @@ e1 *: e2 = Mul e1 e2
 
 
 
+data CondExpression = B Bool | Eq Expression Expression | UnEq Expression Expression | Less Expression Expression | Bigger Expression Expression | LesEq Expression Expression | BigEq Expression Expression | And CondExpression CondExpression | Or CondExpression CondExpression | Not CondExpression deriving Show
+
+infix 4 ==:
+e1 ==: e2 = Eq e1 e2
+
+infix 4 !=:
+e1 !=: e2 = UnEq e1 e2
+
+infix 4 <:
+e1 <: e2 = Less e1 e2
+
+infix 4 <=:
+e1 <=: e2 = LesEq e1 e2
+
+infix 4 >:
+e1 >: e2 = Bigger e1 e2
+
+infix 4 >=:
+e1 >=: e2 = BigEq e1 e2
+
+infixl 2 &&:
+e1 &&: e2 = And e1 e2
+
+infixl 3 ||:
+e1 ||: e2 = Or e1 e2
+
+infixl 9 !:
+(!:) :: CondExpression -> CondExpression
+(!:) = Not
+
+
+
+value :: Variable -> Int
+value (_, v) = v
+
 evalue :: Expression -> State -> Int
 evalue (I v) _ = v
 evalue (V s) state = value $ head $ filter (\(n,_) -> n == s) state
@@ -48,6 +81,17 @@ evalue (Add e1 e2) state = (evalue e1 state) + (evalue e2 state)
 evalue (Sub e1 e2) state = (evalue e1 state) - (evalue e2 state)
 evalue (Mul e1 e2) state = (evalue e1 state) * (evalue e2 state)
 
+evalueCond :: CondExpression -> State -> Bool
+evalueCond (B b) _ = b
+evalueCond (Eq e1 e2) state = (evalue e1 state) == (evalue e2 state)
+evalueCond (UnEq e1 e2) state = (evalue e1 state) /= (evalue e2 state)
+evalueCond (Less e1 e2) state = (evalue e1 state) < (evalue e2 state)
+evalueCond (Bigger e1 e2) state = (evalue e1 state) > (evalue e2 state)
+evalueCond (LesEq e1 e2) state = (evalue e1 state) <= (evalue e2 state)
+evalueCond (BigEq e1 e2) state = (evalue e1 state) >= (evalue e2 state)
+evalueCond (Not cond) state = not $ evalueCond cond state
+evalueCond (And c1 c2) state = (evalueCond c1 state) && (evalueCond c2 state)
+evalueCond (Or c1 c2) state = (evalueCond c1 state) || (evalueCond c2 state)
 
 
 findAndReplace :: Variable -> State -> State
@@ -56,8 +100,15 @@ findAndReplace (s,v) ((sv,vv):xs) = if s==sv then (s,v):xs else (sv,vv):(findAnd
 
 executeInstruction :: Instruction -> State -> State
 executeInstruction (Asign var expr) state = findAndReplace (var,evalue expr state) state
+executeInstruction (While cond ins) state = if (evalueCond cond state) then executeInstruction (While cond ins) (execute ins state) else state
 
 execute :: Program -> State -> State
 execute (ins) s0 = foldl (\x y -> executeInstruction y x) s0 ins
 
 ejecuta=execute
+
+factorial :: Program
+factorial = ["Y" =: V "X","R" =: I 1, While (I 0 <: V "Y") ["R" =: V "R" *: V "Y", "Y" =: V "Y" -: I 1]]
+
+s0 :: State
+s0 = [("X",20)]
